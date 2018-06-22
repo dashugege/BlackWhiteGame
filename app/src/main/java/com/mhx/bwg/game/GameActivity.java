6de3ac780320b3a9dc183d1f39ea18198923f983
hongjiang <hongjiang@soyoung.com>
2018-06-22 13:52:34 +0800

    Initial commit

---
+++ app/src/main/java/com/mhx/bwg/game/GameActivity.java
@@ -0,0 +1,37 @@
+package com.mhx.bwg.game;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+import com.mhx.bwg.R;
+import com.mhx.bwg.view.GameSurfaceView;
+
+public class GameActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getSupportActionBar().hide();
+        GameSurfaceView game = new GameSurfaceView(this);
+        setContentView(game);
+    }
+
+    //设置自定义的参数
+    private void setValues(GameSurfaceView game){
+        //设置初始速度
+        game.setSpeedInit(10);
+        //设置是否加速
+        game.setSpeedUp(true);
+        //设置加速度
+        game.setAcceleration(10);
+        //设置屏幕纵向有几个格子
+        game.setVerticalCount(5);
+        //设置屏幕横向有几个格子
+        game.setHorizontalCount(5);
+    }
+}
---
+++ app/src/main/java/com/mhx/bwg/utils/ToastUtils.java
@@ -0,0 +1,69 @@
+package com.mhx.bwg.utils;
+
+import android.content.Context;
+import android.widget.Toast;
+
+public class ToastUtils
+{
+	private static Toast toast;
+	private ToastUtils()
+	{
+		/* cannot be instantiated */
+		throw new UnsupportedOperationException("cannot be instantiated");
+	}
+
+	public static void showShort(Context context, CharSequence message)
+	{
+		if (toast == null){
+			toast = Toast.makeText(context, message, Toast.LENGTH_SHORT);
+		}else {
+			toast.setText(message);
+		}
+		toast.show();
+	}
+	public static void showShort(Context context, int message){
+		if (toast == null){
+			toast = Toast.makeText(context, message, Toast.LENGTH_SHORT);
+		}else {
+			toast.setText(message);
+		}
+		toast.show();
+	}
+
+	public static void showLong(Context context, CharSequence message)
+	{
+		if (toast == null){
+			toast = Toast.makeText(context, message, Toast.LENGTH_LONG);
+		}else {
+			toast.setText(message);
+		}
+		toast.show();
+	}
+	public static void showLong(Context context, int message){
+		if (toast == null){
+			toast = Toast.makeText(context, message, Toast.LENGTH_LONG);
+		}else {
+			toast.setText(message);
+		}
+		toast.show();
+	}
+
+	public static void show(Context context, CharSequence message, int duration)
+	{
+		if (toast == null){
+			toast = Toast.makeText(context, message, duration);
+		}else {
+			toast.setText(message);
+		}
+		toast.show();
+	}
+	public static void show(Context context, int message ,int duration){
+		if (toast == null){
+			toast = Toast.makeText(context, message, duration);
+		}else {
+			toast.setText(message);
+		}
+		toast.show();
+	}
+
+}
